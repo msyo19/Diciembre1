4ec1fffbbd4eab2dbfa977a89fc9d7e09bb52d7f
Carlos Eli <carloselilopezt@gmail.com>
2018-12-01 14:56:18 -0600

    Models structure

--- app/Brand.php
+++ app/Brand.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Brand extends Model
 {
-    //
+    protected $fillable = ['name', 'description'];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function colors() {
+        return $this->belongsToMany('App\Color');
+    }
+
+    public function photos() {
+        return $this->hasMany('App\Photo');
+    }
 }
