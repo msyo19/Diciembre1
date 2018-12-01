1453bf13ae613e76b1116c4c01152e63e1563b3f
Jorge Alberto Carranza Solis <jorgemsyo17@gmail.com>
2018-12-01 15:18:42 -0600

    Objeto js

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -9,9 +9,34 @@ function ConsultaAjax(ruta){
         })
 }
 
+
+var plantillaBrand = `
+<div class="col-sm">
+<div class="card border-primary mb-3" style="max-width: 25rem;">
+    <div class="card-body">
+        <h5 class="card-title">Brand Title</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <p class="card-text">Text about the brand search </p>
+        <a href="#" class="card-link">More information</a>
+    </div>
+</div>
+</div>
+`
 function CreacionBrand() {
-    if(h != null){
-        console.log("hola");
+    if(hola != null){
+        var plantillaBrand = `
+        <div class="col-sm">
+        <div class="card border-primary mb-3" style="max-width: 25rem;">
+            <div class="card-body">
+                <h5 class="card-title">Brand Title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Text about the brand search </p>
+                <a href="#" class="card-link">More information</a>
+            </div>
+        </div>
+        </div>
+        `
+        let contenedor = document.getElementById('contenedor');
+        contenedor.innerHTML += plantillaBrand;
     }
-    console.log("holas");
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -6,20 +6,10 @@
 <h1>BRANDS</h1>
 
 <div class="container">
-    <div class="row">
-        <div class="col-sm">
-            <div class="card border-primary mb-3" style="max-width: 25rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Brand Title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Text about the brand search </p>
-                    <a href="#" class="card-link">More information</a>
-                </div>
-            </div>
-        </div>
-       
+    <div class="row" id="contenedor">
+        
     </div>
 
 </div>
-
+<script src="js/ajax.js"></script>
 @endsection
