470485c49065743250e0ec32d6236c67e48c54f0
Giancarlos Garza <giancarlosgza@gmail.com>
2018-12-01 16:39:46 -0600

    LANDING  PAGE UPDATE

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top">
     <div class="container">
-        <a class="navbar-brand text-logo" href="#">UBRAND</a>
+        <a class="navbar-brand text-logo" href="/">UBRAND</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
             aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link text-nav" href="#">Inicio <span class="sr-only">(current)</span></a>
+                    <a class="nav-link text-nav" href="/">Inicio <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-nav" href="#">Caracteristicas</a>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,7 +7,10 @@
         <div class="row">
             <div class="col-sm-12 col-md-6 landing-text">
                 <h1 class="text-logo">UBRAND</h1>
-                <h2>Óbten el brand de tu empresa con pocos clicks</h2>
+                <h2>Óbten el brand de tu empresa con pocos clicks.</h2>
+                <br>
+                <a href="/register" class="btn btn-landing-page">CREA UNA CUENTA</a>
+                <a href="#" class="btn btn-landing-page">CARACTERÍSTICAS</a>
             </div>
         </div>
     </div>
