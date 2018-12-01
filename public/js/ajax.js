cc5ade602c0e7fd0b544b2707696974f9b6d8ff2
Jorge Alberto Carranza Solis <jorgemsyo17@gmail.com>
2018-12-01 15:27:42 -0600

    conexion

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -2,26 +2,15 @@ var hola = null;
 function ConsultaAjax(ruta){
     axios.get(ruta)
         .then((response) => {
-            hola =  response.data;
+            hola =  response.result;
         })
         .catch((error) => {
             hola =  error;
         })
 }
+ConsultaAjax('api/brands');
+console.log(hola);
 
-
-var plantillaBrand = `
-<div class="col-sm">
-<div class="card border-primary mb-3" style="max-width: 25rem;">
-    <div class="card-body">
-        <h5 class="card-title">Brand Title</h5>
-        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-        <p class="card-text">Text about the brand search </p>
-        <a href="#" class="card-link">More information</a>
-    </div>
-</div>
-</div>
-`
 function CreacionBrand() {
     if(hola != null){
         var plantillaBrand = `
