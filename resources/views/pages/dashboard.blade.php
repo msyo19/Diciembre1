8c7090c27989862a0dd93408bcd7c01d4105fdca
alienM <alliemichellfs@gmail.com>
2018-12-01 15:10:46 -0600

    CardsBrands

---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.app')
+@section('title', 'Inicio')
+@section('content') 
+
+<!--PONER RUTA DE LAS BRANDS-->
+<h1>BRANDS</h1>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm">
+            <div class="card border-primary mb-3" style="max-width: 25rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Brand Title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Text about the brand search </p>
+                    <a href="#" class="card-link">More information</a>
+                </div>
+            </div>
+        </div>
+       
+    </div>
+
+</div>
+
+@endsection
